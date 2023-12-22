2c0c96ad345cc4413ee7ed3d7e511ecc5d1612c2
gaussx <kmalecki2002@gmail.com>
2023-12-22 22:38:10 +0100

    Add licensing

--- src/main/java/com/sample/CanWeDateUI.java
+++ src/main/java/com/sample/CanWeDateUI.java
@@ -1,3 +1,24 @@
+/*
+    CanWeDate - system ekspercki do decydowania o potencjalnych partnerach romantycznych
+    Copyright (C) 2023, Kamil Małecki, Oskar Kiliańczyk
+
+    Niniejszy program jest wolnym oprogramowaniem; możesz go 
+    rozprowadzać dalej i/lub modyfikować na warunkach Powszechnej
+    Licencji Publicznej GNU, wydanej przez Fundację Wolnego
+    Oprogramowania - według wersji 3-ciej tej Licencji.
+
+    Niniejszy program rozpowszechniany jest z nadzieją, iż będzie on 
+    użyteczny - jednak BEZ JAKIEJKOLWIEK GWARANCJI, nawet domyślnej 
+    gwarancji PRZYDATNOŚCI HANDLOWEJ albo PRZYDATNOŚCI DO OKREŚLONYCH 
+    ZASTOSOWAŃ. W celu uzyskania bliższych informacji - Powszechna 
+    Licencja Publiczna GNU. 
+
+    Z pewnością wraz z niniejszym programem otrzymałeś też egzemplarz 
+    Powszechnej Licencji Publicznej GNU (GNU General Public License);
+    jeśli nie - napisz do Free Software Foundation, Inc., 675 Mass Ave,
+    Cambridge, MA 02139, USA.
+ */
+
 package com.sample;
 
 import java.awt.BorderLayout;
