b7102415c8fd5fa3767ea57be63cf1bd2d2f195f
gaussx <kmalecki2002@gmail.com>
2023-12-22 22:29:40 +0100

    Add everything

---
+++ src/main/java/com/sample/CanWeDate.java
@@ -0,0 +1,15 @@
+package com.sample;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class CanWeDate {
+
+    public static void main(String[] args) {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kc = ks.getKieClasspathContainer();
+        KieSession kSession = kc.newKieSession("ksession-rules");
+        kSession.fireAllRules();
+    }
+}
